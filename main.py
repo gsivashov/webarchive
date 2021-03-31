68ad3d1a364527aff0d2395152996771932cf41a
gsivashov <g.sivashov@gmail.com>
2021-03-31 12:22:51 +0200

    starting new repo

---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+
+from requests_html import HTMLSession
+from argparse import ArgumentParser
+from datetime import datetime
+
+
+def get_response(url):
+    session = HTMLSession()
+    response = session.get(url)
+    # print(url.strip())
+    return response
+
+
+def api_url(url):
+    return f'http://archive.org/wayback/available?url={url}'
+
+
+def main(file):
+    '''
+    https://archive.org/help/wayback_api.php
+    '''
+
+    with open(file) as start_file:
+        for url in start_file:
+            response = get_response(api_url(url.strip()))
+            parse = response.json()
+            if len(parse['archived_snapshots']) != 0:
+                timestamp = parse['archived_snapshots']['closest']['timestamp']
+                formated_date = datetime.strptime(timestamp, "%Y%m%d%H%M%S")
+                print(f'{url.strip()}|{formated_date}')
+            else:
+                print(f'{url.strip()}|no response')
+
+
+if __name__ == '__main__':
+    main('urls.txt')
